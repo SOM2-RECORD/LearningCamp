3d62d753bbe486d13850f1157bc79ec01c540af7
이지희 <som2plestory@naver.com>
2024-10-30 14:23:18 +0900

    Update main.js

--- frontend/Javascript_JSON_LocalStorage/front_ws_04_1/js/main.js
+++ frontend/Javascript_JSON_LocalStorage/front_ws_04_1/js/main.js
@@ -1,5 +1,5 @@
 window.onload = function () {
-  let cars = _________._________("cars");
+  let cars = localStorage.getItem("cars");
 
   let likeCarList = document.querySelector("#like-car-list");
 
@@ -7,7 +7,7 @@ window.onload = function () {
     likeCarList.innerText = "아직 찜한 자동차가 없습니다...";
   } else {
     // 데이터가 있다면 가져온 cars를 Json으로 변경하기
-    cars = JSON._________(cars);
+    cars = JSON.parse(cars);
 
     let likeCarListHtml = document.createElement("ul");
     for (let i = 0; i < cars.length; i++) {
@@ -32,18 +32,18 @@ btnList.forEach((btn) => {
     };
 
     // 로컬스토리지에 있는 cars 를 가져온다.
-    let localCars = _________._________("cars");
+    let localCars = localStorage.getItem("cars");
 
     if (localCars === null) {
       localCars = [cars];
     } else {
       // cars가 있다면 Json 으로 변경
-      localCars = JSON._________(localCars);
+      localCars = JSON.parse(localCars);
       localCars.push(cars);
     }
 
     // 로컬스토리지에 저장하기 위해서는 문자열의 형태로만 저장할 수 있다.
-    const carsJson = JSON._________(localCars);
-    _________._________("cars", carsJson);
+    const carsJson = JSON.stringify(localCars);
+    localStorage.setItem("cars", carsJson);
   });
 });
