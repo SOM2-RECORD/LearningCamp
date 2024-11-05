d73d96a58cc35db960acb2099aec44819c1fa89e
som2 <som2plestory@naver.com>
2024-11-06 01:35:05 +0900

    create

---
+++ spring/SpringBoot_RestAPI_Swagger/spring_hw_10_2/src/main/java/com/ssafy/hw/SpringHw102Application.java
@@ -0,0 +1,13 @@
+package com.ssafy.hw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHw102Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHw102Application.class, args);
+	}
+
+}
