26e61cbd1285c5d05d45c228859b586b8dc58d58
이지희 <som2plestory@naver.com>
2024-11-07 18:07:17 +0900

    create

---
+++ vuejs/Single_File_Components/vue_hw_4_4/my-first-front-project-mk2/src/App.vue
@@ -0,0 +1,7 @@
+<template>
+  <ColorChange/>
+</template>
+
+<script setup>
+import ColorChange from '@/components/ColorChange.vue';
+</script>
---
+++ vuejs/Single_File_Components/vue_hw_4_4/my-first-front-project-mk2/src/components/ColorChange.vue
@@ -0,0 +1,21 @@
+<template>
+    <input v-model="color" type="text"/>
+    <p :class="color">입력창에 올바른 색상 명을 입력하면 글자색이 바껴요.</p>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+const color = ref('');
+</script>
+
+<style scoped>
+.red{
+    color:red;
+}
+.blue{
+    color:blue;
+}
+.green{
+    color:green;
+}
+</style>
