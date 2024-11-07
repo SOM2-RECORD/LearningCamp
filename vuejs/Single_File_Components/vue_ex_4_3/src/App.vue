706d843d14ebf2d5d2b58b6575a8f2ede8e603ee
이지희 <som2plestory@naver.com>
2024-11-07 15:33:45 +0900

    update

--- vuejs/Single_File_Components/vue_ex_4_3/src/App.vue
+++ vuejs/Single_File_Components/vue_ex_4_3/src/App.vue
@@ -1,9 +1,12 @@
 <template>
-  <MyComponent />
+  <Header/>
+  <GalleryList/>
 </template>
 
 <script setup>
-import MyComponent from '@/components/MyComponent.vue'
+// import MyComponent from '@/components/MyComponent.vue'
+import Header from '@/components/Header.vue'
+import GalleryList from '@/components/GalleryList.vue'
 </script>
 
 <style>
---
+++ vuejs/Single_File_Components/vue_ex_4_3/src/components/GalleryList.vue
@@ -0,0 +1,20 @@
+<template>
+
+    <main class="container py-3">
+      <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+        <div class="col" v-for="_ in numberOfItems">
+          <div class="card">
+            <img src="@/assets/sample.png" class="card-img-top" alt="...">
+            <GalleryListItem/>
+          </div>
+        </div>
+      </div>
+    </main>
+</template>
+
+<script setup>
+import GalleryListItem  from '@/components/GalleryListItem.vue'
+
+const numberOfItems = 4
+</script>
+
