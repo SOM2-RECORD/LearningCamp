03877ca7326402e6eb23fe3a18e6cc6e326df4bc
이지희 <som2plestory@naver.com>
2024-11-07 16:51:49 +0900

    update

---
+++ vuejs/Single_File_Components/vue_ws_4_4/src/components/SingerList.vue
@@ -0,0 +1,31 @@
+<template>
+    <h1 class="text-center color-orange">가수 목록</h1>
+    <div class="card-container text-center">
+        <div class="card solid-border" v-for="_ in singerCount" :key="_">
+            <SingerDetail/>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import SingerDetail from '@/components/singers/SingerDetail.vue';
+
+const singerCount = 4;
+</script>
+
+<style scoped>
+.color-orange{
+    color:orange;
+}
+
+.solid-border{
+    border: 1px solid;
+}
+.card{
+    display: inline-block;
+    width: 40%;
+    margin: 20px;
+    padding : 20px;
+    box-sizing: border-box;
+}
+</style>
