a4a60c8780d29002049e0d60601eedf3805fe04c
som2 <som2plestory@naver.com>
2024-11-15 16:24:06 +0900

    update

---
+++ vuejs/Component_State_Flow/vue_ws_5_3/src/components/BusinessCard.vue
@@ -0,0 +1,70 @@
+<script setup>
+import {ref} from 'vue'
+import BusinessCardDetail from './businessCardDetail.vue';
+
+let id = 0;
+
+const businessCards = ref([
+    {
+        id : id++,
+        name : "일론 머스크",
+        title : "테슬라 테크노킹"
+    },
+    {
+        id : id++,
+        name : "래리 엘리슨",
+        title : "오라클 창업주"
+    },
+    {
+        id : id++,
+        name : "빌 게이츠",
+        title : "마이크로소프트 공동창업주"
+    },
+    {
+        id : id++,
+        name : "래리 페이지",
+        title : "구글 공동창업주"
+    },
+    {
+        id : id++,
+        name : "세르게이브린",
+        title : "구글 공동창업주"
+    },
+])
+
+const removeCard = (cardId) => {
+    businessCards.value = businessCards.value.filter(item => item.id !== cardId)
+}
+
+</script>
+
+<template>
+<div class="cardlist">
+    <div class="card" 
+        v-for="businessCard in businessCards" 
+        :key="businessCard.id"
+        >
+        <BusinessCardDetail 
+        :businessCard = "businessCard"
+        @remove-card="removeCard"/>
+    </div>
+</div>
+</template>
+<style scoped>
+.cardlist {
+    text-align: center;
+    min-height: 150px;
+    display: flex;
+    flex-wrap: wrap;
+    gap: 20px;
+    justify-content: center;
+}
+
+.card{
+    background-color: rgb(247, 247, 247);
+    width: 35%;
+    padding : 10px;
+    margin: 10px 0;
+    border: 1px solid gainsboro;
+}
+</style>
---
+++ vuejs/Component_State_Flow/vue_ws_5_3/src/components/businessCardDetail.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { defineProps, defineEmits } from 'vue'
+
+const props = defineProps({
+    businessCard:{
+        id : Number,
+        name : String,
+        title : String
+    }
+})
+
+const emit = defineEmits(['remove-card'])
+
+const removeCard = function(cardId){
+    emit('remove-card', cardId)
+}
+
+</script>
+
+
+<template>
+    <h3>이름 : {{businessCard.name}}</h3>
+    <p>직함 : {{businessCard.title}}</p>
+    <button @click="$emit('remove-card', businessCard.id)">명함 삭제</button>
+</template>
