044601ac702cc2c3a918e9f1589be9cfcdc15d19
som2 <som2plestory@naver.com>
2024-11-15 16:49:07 +0900

    update

--- vuejs/Component_State_Flow/vue_ws_5_4/src/App.vue
+++ vuejs/Component_State_Flow/vue_ws_5_4/src/App.vue
@@ -1,10 +1,54 @@
 <script setup>
+
+import BusinessCard from './components/BusinessCard.vue';
+
 </script>
 
 <template>
-  
+  <header>
+    <div>
+      <h1>명함 관리 페이지</h1>
+    </div>
+  </header>
+  <main>
+    <p>명함을 관리하는 페이지입니다. 여기에 명함 목록이 표시됩니다.</p>
+  </main>
+
+  <article>
+    <h1>보유 명함 목록</h1>
+    <BusinessCard/>
+  </article>
+
+  <footer>
+    ⓒ 2024 My Business Cards
+  </footer>
+
+
 </template>
 
-<style scoped>
+<style>
 
+header {
+    margin: 0 auto;
+    text-align: center;
+    width: 350px;
+    padding: 20px;
+    color: white;
+    background-color: rgb(68, 122, 240);
+}
+
+main {
+    text-align: center;
+}
+
+article {
+    text-align: center;
+}
+
+footer {
+    padding: 20px;
+    font-weight: 700;
+    text-align: center;
+    background-color: rgb(241, 239, 239);
+}
 </style>
---
+++ vuejs/Component_State_Flow/vue_ws_5_4/src/components/BusinessCard.vue
@@ -0,0 +1,72 @@
+<script setup>
+import {ref} from 'vue'
+import BusinessCardDetail from './businessCardDetail.vue';
+
+let id = 0;
+
+const businessCards = ref([
+    {
+        id : id++,
+        name : "일론 머스크",
+        title : "테슬라 테크노킹"
+    },
+    {
+        id : id++,
+        name : "래리 엘리슨",
+        title : "오라클 창업주"
+    },
+    {
+        id : id++,
+        name : "빌 게이츠",
+        title : "마이크로소프트 공동창업주"
+    },
+    {
+        id : id++,
+        name : "래리 페이지",
+        title : "구글 공동창업주"
+    },
+    {
+        id : id++,
+        name : "세르게이브린",
+        title : "구글 공동창업주"
+    },
+])
+
+const removeCard = (cardId) => {
+    businessCards.value = businessCards.value.filter(item => item.id !== cardId)
+}
+
+</script>
+
+<template>
+<p v-if="businessCards.length > 0">현재 보유중인 먕함 수 : {{businessCards.length}}</p>
+<p v-if="businessCards.length == 0">명함이 없습니다. 새로운 명함을 추가해 주세요.</p>
+<div class="cardlist">
+    <div class="card" 
+        v-for="businessCard in businessCards" 
+        :key="businessCard.id"
+        >
+        <BusinessCardDetail 
+        :businessCard = "businessCard"
+        @remove-card="removeCard"/>
+    </div>
+</div>
+</template>
+<style scoped>
+.cardlist {
+    text-align: center;
+    min-height: 150px;
+    display: flex;
+    flex-wrap: wrap;
+    gap: 20px;
+    justify-content: center;
+}
+
+.card{
+    background-color: rgb(247, 247, 247);
+    width: 35%;
+    padding : 10px;
+    margin: 10px 0;
+    border: 1px solid gainsboro;
+}
+</style>
